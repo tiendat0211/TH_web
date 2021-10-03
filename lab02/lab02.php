4ea7c54c5a536e1a5523f6bec60d058b146b28dd
tiendat0211 <79643933+tiendat0211@users.noreply.github.com>
2021-10-03 14:47:08 +0700

    commit

--- lab02/lab02.php
+++ lab02/lab02.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Receiving Input</title>
+    <link rel="stylesheet" href="style2.css">
 </head>
 <body>
 
@@ -13,11 +14,22 @@
         $name = $_GET["name"];
         $class = $_GET["class"];
         $university= $_GET["university"];
-        $hobby = $_GET["hobby"];
-        print("<br>Hello, $name");
-        print("<br>You are studying at $class, $university");
-        echo "<br>Your hobby is:";
-        echo "$hobby";
+        $hobbies = $_GET["hobby"];
+        $gender=$_GET["gender"];
+        print("<br>  Hello, $name");
+        print ("<br> Gender: $gender");
+        print("<br>  You are studying at $class, $university");
+        echo "<br>   Your hobby is:";
     ?>
+        <div class="row">
+            <?php
+            $i=1;
+            foreach($hobbies as $selected){
+                print ("$i. $selected<br>");
+                $i++;
+            }
+            ?>
+        </div>
+
 </body>
 </html>
